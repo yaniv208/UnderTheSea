6a4225e8fc757b4b167d1c67dbfc4cb246aa92e9
Nissim Pardo <androdogs@gmail.com>
2021-06-17 22:32:49 +0300

    main page

--- app/src/main/java/com/hit/underthesea/MainActivity.java
+++ app/src/main/java/com/hit/underthesea/MainActivity.java
@@ -2,7 +2,9 @@ package com.hit.underthesea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView=findViewById(R.id.bubble1);
+        AnimationDrawable animationDrawable =(AnimationDrawable)imageView.getDrawable();
+        animationDrawable.start();
+        ImageView imageView2=findViewById(R.id.bubble2);
+        AnimationDrawable animationDrawable2 =(AnimationDrawable)imageView2.getDrawable();
+        animationDrawable2.start();
     }
+
+
 }

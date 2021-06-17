b2a6de9395050717fd3e14dc834bdf158f1ae0cc
Nissim Pardo <androdogs@gmail.com>
2021-06-17 20:02:34 +0300

    Initial commit

---
+++ app/src/main/java/com/hit/underthesea/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hit.underthesea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
